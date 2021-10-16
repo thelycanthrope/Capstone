5c5aa738628900a28ff04e6c4a5efefdb7dffc83
thelycanthrope <69922095+thelycanthrope@users.noreply.github.com>
2021-10-16 13:46:24 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import AgeCalci from './AgeCalci';
+import AgeCalculator from './AgeCalculator';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      
+     {/* <AgeCalci/> */}
+     <AgeCalculator/>
+    </div>
+  );
+}
+
+export default App;
